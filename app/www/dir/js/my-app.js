4ca8f4177c3cd2622b6791f4402665147f25725e
jonathantobi <jonathan_tobi@live.com>
2017-01-30 01:23:32 -0300

    home aangepast

--- app/www/dir/js/my-app.js
+++ app/www/dir/js/my-app.js
@@ -17,6 +17,7 @@ myApp.onPageInit('index', function (page) {
   mainView.router.loadPage('views/home.html'); 
 }).trigger();
 
+
 // onPageInit
 myApp.onPageInit('home', function (page) {
   	console.log('home initialized');
@@ -81,13 +82,6 @@ myApp.onPageAfterAnimation('home', function (page) {
     $$('.page-content').toggleClass('legende-open');
   })
 });
-var myApp = new Framework7(); 
- 
-  // Init slider and store its instance in mySwiper variable
-  var mySwiper = myApp.swiper('.swiper-container', {
-    pagination:'.swiper-pagination'
-  });
-// calender
 
 
 
@@ -101,5 +95,70 @@ myApp.onPageAfterAnimation('account', function (page) {
   	console.log('go to login');
   	mainView.router.loadPage('views/login.html'); 
   }
+});
+
+myApp.onPageInit('login', function (page) {
+  console.log('login init');
+
+  $$('#login-form').submit( function(e){
+    e.preventDefault();
+
+    var formData = myApp.formToJSON('#login-form');
+    // console.log(JSON.stringify(formData));
+  
+  });
+});
+
+
+myApp.onPageInit('aanmelden', function (page) {
+  console.log('aanmelden init');
+
+  $$('#aanmelden_error, #password_error, #password_conf_error').hide();
+
+    var pass = $$('#password'), 
+    pass_conf = $$('#password_conf'), 
+    name = $$('.name');
+    sub = $$('input[type=submit');
+
+  // allow only letters
+  name.on('keyup', function(){
+    var transformedInput = $$(this).val().replace(/[^a-zA-Z\s]/g, '');  
+
+    if (transformedInput !== $$(this).val()) {
+      $$(this).val(transformedInput);
+    } 
+  });
+
+  // password longer than 6 chars
+  pass.on('keyup change', function(){
+    if (pass.val().length < 6) {
+      console.log(pass.val().length);
+      $$('#password_error').show();
+      sub.addClass('disabled');
+    }else{
+      $$('#password_error').hide();
+      sub.removeClass('disabled');
+    }   
+  }); 
+
+  // check if same as password
+  pass_conf.on('keyup change', function(){
+    if (pass.val() != pass_conf.val()) {
+      $$('#password_conf_error').show();
+      sub.addClass('disabled');
+    }else{
+      $$('#password_conf_error').hide();
+      sub.removeClass('disabled');
+    } 
+  });
+
+  $$('#aanmelden-form').submit( function(e){
+    e.preventDefault();
+
+    var formData = myApp.formToJSON('#aanmelden-form');
+    console.log(JSON.stringify(formData));
+
+
+  });
 });
 
