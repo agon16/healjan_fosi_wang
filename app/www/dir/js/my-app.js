34e4f75ce7850889fbb00bc79e76f6c7a823452a
timmy1420 <tim.poco01@gmail.com>
2017-02-05 21:50:20 -0300

    OSM script aangepast

--- app/www/dir/js/my-app.js
+++ app/www/dir/js/my-app.js
@@ -23,54 +23,25 @@ myApp.onPageInit('home', function (page) {
   	console.log('home initialized');
 
   	//Init map
+  var map = L.map('map').setView([4.916,-55.042], 5);
 
-  	var map = L.map('map').setView([5.7481,-55.0988], 11);
+   // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+   //   // L.tileLayer('../../../../../../tiles/', {
+   //     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+   // }).addTo(map);
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+   // L.marker([5.7481,-55.0988]).addTo(map)
+   //     .bindPopup('Commewijne passie dja.')
+   //     .openPopup();
+ 
+  //Custom script for Stack overflow
+  L.tileLayer('dir/tiles/{z0}/{x0}/{x1}/{y0}/{y1}.png').addTo(map); //gMapCatcher
 
-L.marker([5.7481,-55.0988]).addTo(map)
-    .bindPopup('Commewijne passie dja.')
-    .openPopup();
-
-	// var coolPlaces = new L.LayerGroup();
- //    L.marker([-41.29042, 174.78219])
- //        .bindPopup('Te Papa').addTo(coolPlaces),
- //    L.marker([-41.29437, 174.78405])
- //        .bindPopup('Embassy Theatre').addTo(coolPlaces),
- //    L.marker([-41.2895, 174.77803])
- //        .bindPopup('Michael Fowler Centre').addTo(coolPlaces),
- //    L.marker([-41.28313, 174.77736])
- //        .bindPopup('Leuven Belgin Beer Cafe').addTo(coolPlaces),
- //    L.polyline([
- //        [-41.28313, 174.77736],
- //        [-41.2895, 174.77803],
- //        [-41.29042, 174.78219],
- //        [-41.29437, 174.78405]
- //        ]
- //        ).addTo(coolPlaces);
- //    var osmLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>',
- //        thunLink = '<a href="http://thunderforest.com/">Thunderforest</a>';
-    
- //    var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
- //        osmAttrib = '&copy; ' + osmLink + ' Contributors',
- //        landUrl = 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png',
- //        thunAttrib = '&copy; '+osmLink+' Contributors & '+thunLink;
- //    var osmMap = L.tileLayer(osmUrl, {attribution: osmAttrib}),
- //        landMap = L.tileLayer(landUrl, {attribution: thunAttrib});
- //    var map = L.map('map', {
-	// 	    layers: [osmMap] // only add one!
-	//     })
-	//     .setView([-41.2858, 174.78682], 14);
-	// var baseLayers = {
-	// 	"OSM Mapnik": osmMap,
-	// 	"Landscape": landMap
-	// };
-	// var overlays = {
-	// 	"Interesting places": coolPlaces
-	// };
-	// L.control.layers(baseLayers,overlays).addTo(map);
+  L.tileLayer('dir/tiles/{z}/{x}/{y}.png').addTo(map);
+  //other tiles
+  L.marker([5.7481,-55.0988]).addTo(map)
+       .bindPopup('Commewijne passie dja.')
+       .openPopup();
 
 });
 
